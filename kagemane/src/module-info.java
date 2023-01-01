699ce81c37fd11a782c8c406445e21e0b8eb1a90
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-01-01 15:20:14 -0300

    refactor: Lesson 366 - Package export for a specific module in the module-info file.

--- kagemane/src/module-info.java
+++ kagemane/src/module-info.java
@@ -3,4 +3,6 @@ module kagemane {
 //    requires transitive kagebunshin;
     exports kagemane.main.java.model.external;
 
+    exports kagemane.main.java.model.internal
+        to kagezukami;
 }
--- kagezukami/src/kagezukami/test/java/model/TestCalculator.java
+++ kagezukami/src/kagezukami/test/java/model/TestCalculator.java
@@ -2,6 +2,7 @@ package kagezukami.test.java.model;
 
 
 import kagemane.main.java.model.external.Calculator;
+import kagemane.main.java.model.internal.ArithmeticOperations;
 
 public class TestCalculator {
 
@@ -10,5 +11,7 @@ public class TestCalculator {
         Calculator calculator = new Calculator();
         System.out.println(calculator.sum(2,3,4));
 
+        ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
+        System.out.println(arithmeticOperations.sum(3,3,3));
     }
 }
